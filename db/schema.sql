196ec715bbc8503bba45f124686feb0d6644abb4
Maddox Twin1 <jaspertena@aol.com>
2022-04-24 23:07:13 -0400

    add employee names

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,23 @@ DROP TABLE IF EXISTS EMPLOYEE;
 
 CREATE TABLE DPT(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    department varchar(30) NOT NULL,
-)
+    department VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE ROLES(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title varchar(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary decimal(10,2) NOT NULL,
     department_id INTEGER,
     CONSTRAINT fk_department foreign key (department_id) references DPT(id)
-)
+);
 
 CREATE TABLE EMPLOYEE(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name varchar(30) NOT NULL,
-    last_name varchar(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    CONSTRAINT fk_role_id FOREIGN KEY (role_id) references ROLES(id)
-    manager_id INTEGER
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) references ROLES(id),
+    manager_id INTEGER,
     CONSTRAINT fk_manager_id FOREIGN KEY(manager_id) references EMPLOYEE(id)
-)
+);
