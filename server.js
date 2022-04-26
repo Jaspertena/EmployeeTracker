5963e06f50b84f274db8487b4783479e6b818ac8
Maddox Twin1 <jaspertena@aol.com>
2022-04-26 19:16:45 -0400

    added tables

--- server.js
+++ server.js
@@ -1,60 +1,55 @@
 // calling the express library and requiring
 // const { application } = require('express');
-const inquire = require('inquire');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
-
+const db = require('./db/connection')
 
 const fs = require('fs');
+function startapp(){
+  return inquirer
+      .prompt([
+          {
+              type: 'list',
+              message: 'What would you like today?',
+              name: 'roles',
+              choices: ["all departments", "all roles", "all employees", "add department", "add role", "add employee", "exit"]
+            },  
+          ])
+          .then (choice => {
+            console.log(choice)
+            if(choice.roles==="all departments"){
+              viewdpt()
+            }
+            else if(choice.roles==="all roles"){
+              viewRoles()
+            }
+            else if(choice.roles==="all employees"){
+              viewEmp()
+            }
+           
+          })
+}
+function viewdpt(){
+  const sql = `SELECT * FROM DPT`;
+  db.query (sql,(error, res)=>{
+    console.table(res)
+  })
+}
 
-inquirer
-    .prompt([
-        {
-            type: 'checkbox',
-            message: 'What would you like today?',
-            name: 'roles',
-            choices: ["all departments", "all roles", "all employees"]
-          },
-          {
-            type: 'input',
-            message: 'Department Name?',
-            name: 'department_ID',
-          },
-          {
-            type: 'input',
-            message: 'What is the role?',
-            name: 'title',
-          },
-          {
-            type: 'input',
-            message: 'What is the starting salary?',
-            name: 'salary',
-          },
-          {
-            type: 'input',
-            message: 'Employee first name?',
-            name: 'first_name',
-          },
-          {
-            type: 'input',
-            message: 'Employee last name?',
-            name: 'last_name',
-          },
-          {
-            type: 'input',
-            message: "What is the employee's role ID?",
-            name: 'role_id',
-          },
-          {
-            type: 'input',
-            message: "Who is the employee's manager?",
-            name: 'manager_id',
-          },
-        ])
+function viewRoles(){
+  const sql = `SELECT * FROM ROLES`;
+  db.query (sql,(error, res)=>{
+    console.table(res)
+  })
+}
 
+function viewEmp(){
+  const sql =`SELECT * FROM EMPLOYEE`;
+  db.query (sql, (error, res)=>{
+    console.table(res)
+  })
+}
 
 
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+startapp()
