89c5069d3886e55c69ee46405ba53f6bbf5b004a
Maddox Twin1 <jaspertena@aol.com>
2022-04-26 18:37:50 -0400

    add server.js

--- server.js
+++ server.js
@@ -5,6 +5,52 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 
 
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'checkbox',
+            message: 'What would you like today?',
+            name: 'roles',
+            choices: ["all departments", "all roles", "all employees"]
+          },
+          {
+            type: 'input',
+            message: 'Department Name?',
+            name: 'department_ID',
+          },
+          {
+            type: 'input',
+            message: 'What is the role?',
+            name: 'title',
+          },
+          {
+            type: 'input',
+            message: 'What is the starting salary?',
+            name: 'salary',
+          },
+          {
+            type: 'input',
+            message: 'Employee first name?',
+            name: 'first_name',
+          },
+          {
+            type: 'input',
+            message: 'Employee last name?',
+            name: 'last_name',
+          },
+          {
+            type: 'input',
+            message: "What is the employee's role ID?",
+            name: 'role_id',
+          },
+          {
+            type: 'input',
+            message: "Who is the employee's manager?",
+            name: 'manager_id',
+          },
+        ])
 
 
 
