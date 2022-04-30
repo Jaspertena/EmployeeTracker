3bad5573bec174bc6bdf920faca6132ac6212fdc
Maddox Twin1 <jaspertena@aol.com>
2022-04-29 21:10:12 -0400

    correct all functions

--- server.js
+++ server.js
@@ -3,7 +3,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
-const db = require('./db/connection')
+// const db = require('./db/connection')
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', 
+  database: 'employee_db'
+})
 
 const fs = require('fs');
 function startapp(){
@@ -13,66 +19,33 @@ function startapp(){
               type: 'list',
               message: 'What would you like to do today?',
               name: 'roles',
-              choices: ["View all departments", "View all roles", "View all employees", "add department", "add role", "add employee", "exit"]
+              choices: ["View all departments", "View all roles", "View all employees", "add department", "add role", "add employee", "Exit"]
             },  
           ])
           .then (choice => {
             if(choice.roles==="View all departments"){
-              viewdpt()
-              // askAgain()
+              viewDpt()
             }
             else if(choice.roles==="View all roles"){
               viewRoles()
-              // askAgain()
             }
             else if(choice.roles==="View all employees"){
               viewEmp()
-              // askAgain()
             }
             else if(choice.roles==="add department"){
               enterDpt()
-              // askAgain()
             }
             else if(choice.roles==="add employee"){
               enterEmp()
-              // askAgain()
             }
             else if(choice.roles==="add role"){
               enterRole()
-              // askAgain()
             }
             else if(choice.roles==="Exit"){
-              quit()
+              return askAgain();
             }
-          })
-
-          
-          
+          })         
 }
-// .then((answer) => {
-//             return inquirer
-//             .prompt([
-//                 {
-//                     type: 'input',
-//                     message: 'What is the title for the new Role? ',
-//                     name: 'title',
-//                   },  
-//                   {
-//                     type: 'input',
-//                     message: 'What is the starting salary?',
-//                     name: 'salary',
-//                   },  
-//                   {
-//                     type: 'input',
-//                     message: 'What is the DepartmentID for this new role?',
-//                     name: 'deptId',
-//                   },  
-//                 ])
-          //       .then((answer) => {
-          //         console.log('this is my answer', answer)
-          //           addRole(answer)
-          //       })
-          // })
 function enterRole(){
   return inquirer
   .prompt([
@@ -89,9 +62,16 @@ function enterRole(){
         {
           type: 'input',
           message: 'What is the DepartmentID for this new role?',
-          name: 'deptId',
+          name: 'dept_id',
         },  
       ])
+      .then(choice => {
+        const sql = `INSERT INTO ROLES (title, salary, department_id) VALUES (?, ?, ?)`; 
+        db.query (sql,[choice.title, choice.salary, choice.dept_id],(error, res)=>{
+          console.table(res)
+          return askAgain()
+        })      
+      })
 }
 
 function enterEmp(){
@@ -99,26 +79,57 @@ function enterEmp(){
   .prompt([
       {
           type: 'input',
-          message: 'What is the title for the new Role? ',
-          name: 'title',
+          message: "Enter Employee's first name.",
+          name: 'first_name',
         },  
         {
           type: 'input',
-          message: 'What is the starting salary?',
-          name: 'salary',
+          message: "Enter Employee's last name.",
+          name: 'last_name',
         },  
         {
           type: 'input',
-          message: 'What is the DepartmentID for this new role?',
-          name: 'deptId',
+          message: "Enter Employee's role ID.",
+          name: 'role_id',
+        },  
+        {
+          type: 'input',
+          message: "Enter Employee's Manager ID.",
+          name: 'manager_id',
+        }, 
+      ])
+      .then(choice => {
+        const sql = `INSERT INTO EMPLOYEE(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`; 
+        db.query (sql,[choice.first_name, choice.last_name, choice.role_id, choice.manager_id], (error, res)=>{
+          console.table(res)
+          return askAgain()
+        })
+      
+      })
+}
+function enterDpt(){
+  return inquirer
+  .prompt([
+      {
+          type: 'input',
+          message: "Enter Department name.",
+          name: 'DPT',
         },  
       ])
+      .then(choice => {
+        const sql = `INSERT INTO DPT(Department) VALUES (?)`; 
+        db.query (sql,[choice.DPT],(error, res)=>{
+          console.table(res)
+          return askAgain()
+        })    
+      })
 }
 
-function viewdpt(){
+function viewDpt(){
   const sql = `SELECT * FROM DPT`;
   db.query (sql,(error, res)=>{
     console.table(res)
+    return askAgain()
   })
 }
 
@@ -126,6 +137,7 @@ function viewRoles(){
   const sql = `SELECT * FROM ROLES`;
   db.query (sql,(error, res)=>{
     console.table(res)
+    return askAgain()
   })
 }
 
@@ -133,13 +145,7 @@ function viewEmp(){
   const sql =`SELECT * FROM EMPLOYEE`;
   db.query (sql, (error, res)=>{
     console.table(res)
-  })
-}
-
-function addRole(choice){
-  const sql = `INSERT INTO ROLES(title, salary, department_id) VALUES (${choice.title}, ${choice.salary}, ${choice.deptId})`; 
-  db.query (sql,(error, res)=>{
-    console.table(res)
+    return askAgain()
   })
 }
 
@@ -150,15 +156,8 @@ function addRole(choice){
     })
 }
 
-function addEmpt(choice){
-  const sql = `INSERT INTO EMPLOYEE(first_name, last_name, role_id, manager_id) VALUES (${choice.first_name}, ${choice.last_name}, ${choice.role_id}, ${choice.manager_id})`; 
-  db.query (sql,(error, res)=>{
-    console.table(res)
-  })
-}
-
-async function askAgain(){
-await inquirer 
+function askAgain(){
+inquirer 
   .prompt ([
     {
       type: "confirm",
